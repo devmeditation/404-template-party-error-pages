cbd46c5566d08f36977c76d30f31c7d0149d899d
Dev Meditation <devmeditation@bcl-st.com.br>
2022-12-12 18:18:04 -0300

    404 Template Party Error Page - Paginha de erro 404 modelo festa

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import { _404TemplatePartyErrorPages } from './components/404TemplatePartyErrorPages/404TemplatePartyErrorPages'
+
+function App() {
+  return (
+    <>
+    <_404TemplatePartyErrorPages />
+    </>
+  )
+}
+
+export default App
